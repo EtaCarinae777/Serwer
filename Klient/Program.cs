c1fb35f7c0c998ffc095c35998c8b022cd76de70
EtaCarinae777 <128357960+EtaCarinae777@users.noreply.github.com>
2026-03-23 21:34:03 +0100

    Dodaj pliki projektów.

---
+++ Klient/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+
+class Client
+{
+    static List<string> adresySerwera = new List<string>
+    {
+        "127.0.0.1"
+        // "192.168.1.x" // drugi komputer
+        // "192.168.1.y" // trzeci komputer
+    };
+    static int port = 8001;
+    static int n = 4;
+    static int grainSize = 1;
+    static double prog = 30.0;
+
+    static void Main(string[] args)
+    {
+        // na razie wpisujemy teksty z klawiatury
+        Console.WriteLine("Wpisz pierwszy tekst i nacisnij Enter:");
+        string tekst1 = Console.ReadLine();
+
+        Console.WriteLine("Wpisz drugi tekst i nacisnij Enter:");
+        string tekst2 = Console.ReadLine();
+
+        // wysylamy do serwera i odbieramy wynik
+        WyslijTeksty(adresySerwera[0], tekst1, tekst2);
+
+        // pozniej tutaj bedzie:
+        // List<string> pliki = WczytajIposortuj(args);
+        // List<(string, string)> pary = GenerujPary(pliki);
+        // List<(string, string, double)> wyniki = RozdzielZadania(pary, pliki);
+        // WyswietlMacierz(wyniki, pliki);
+    }
+
+    // na razie wysyla dwa teksty i odbiera liczbe slow
+    // pozniej zastapiona przez WyslijZadanie()
+    static void WyslijTeksty(string adres, string tekst1, string tekst2)
+    {
+        try
+        {
+            Console.WriteLine("[KLIENT] Lacze sie z serwerem " + adres + ":" + port + "...");
+            TcpClient klient = new TcpClient(adres, port);
+            Console.WriteLine("[KLIENT] Polaczono!");
+            Thread.Sleep(500);
+
+            NetworkStream stream = klient.GetStream();
+            BinaryReader reader = new BinaryReader(stream);
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            Console.WriteLine("[KLIENT] Wysylam parametry (n=" + n + " grainSize=" + grainSize + ")...");
+            writer.Write(n);
+            writer.Write(grainSize);
+            Thread.Sleep(500);
+
+            Console.WriteLine("[KLIENT] Wysylam tekst 1...");
+            writer.Write(tekst1);
+            Thread.Sleep(500);
+
+            Console.WriteLine("[KLIENT] Wysylam tekst 2...");
+            writer.Write(tekst2);
+            Thread.Sleep(500);
+
+            Console.WriteLine("[KLIENT] Czekam na wyniki od serwera...");
+            int iloscSlow1 = reader.ReadInt32();
+            int iloscSlow2 = reader.ReadInt32();
+
+            Console.WriteLine("[KLIENT] Otrzymalem wyniki!");
+            Console.WriteLine("[KLIENT] Tekst 1 ma slow: " + iloscSlow1);
+            Console.WriteLine("[KLIENT] Tekst 2 ma slow: " + iloscSlow2);
+
+            klient.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Blad: " + e.Message);
+        }
+    }
+
+    // wysyla jeden plik przez siec
+    static void WyslijPlik(BinaryWriter writer, string sciezka)
+    {
+        byte[] dane = File.ReadAllBytes(sciezka);
+        string nazwa = System.IO.Path.GetFileName(sciezka);
+
+        writer.Write(nazwa);
+        writer.Write((long)dane.Length);
+        writer.Write(dane);
+    }
+
+    // wczytuje pliki podane jako argumenty i sortuje wg rozmiaru
+    static List<string> WczytajIposortuj(string[] args)
+    {
+        // TODO: sprawdzic czy kazdy plik istnieje
+        // TODO: posortowac po File.ReadAllBytes(p).Length malejaco
+        return new List<string>(args);
+    }
+
+    // generuje liste wszystkich unikalnych par plikow
+    static List<(string, string)> GenerujPary(List<string> pliki)
+    {
+        // TODO: podwona petla for i<j zeby nie powtarzac par
+        return new List<(string, string)>();
+    }
+
+    // rozdziela pary miedzy dostepne serwery
+    static List<(string, string, double)> RozdzielZadania(List<(string, string)> pary, List<string> pliki)
+    {
+        // TODO: podzielic pary na paczki wg grainSize
+        // TODO: przydzielic paczki do serwerow round-robin
+        // TODO: wyslac zadania wspolbieznie
+        // TODO: zebrac wyniki i zwrocic
+        return new List<(string, string, double)>();
+    }
+
+    // wysyla dwa pliki do serwera i odbiera wynik podobienstwa
+    static (string, string, double) WyslijZadanie(string adres, string plikA, string plikB)
+    {
+        try
+        {
+            TcpClient klient = new TcpClient(adres, port);
+            NetworkStream stream = klient.GetStream();
+            BinaryReader reader = new BinaryReader(stream);
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            writer.Write(n);
+            writer.Write(grainSize);
+
+            WyslijPlik(writer, plikA);
+            WyslijPlik(writer, plikB);
+
+            string nazwaA = reader.ReadString();
+            string nazwaB = reader.ReadString();
+            double podobienstwo = reader.ReadDouble();
+
+            klient.Close();
+            return (nazwaA, nazwaB, podobienstwo);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Blad polaczenia z " + adres + ": " + e.Message);
+            // TODO: ponowna proba z innym serwerem
+            return (plikA, plikB, -1);
+        }
+    }
+
+    // wyswietla wyniki w konsoli
+    static void WyswietlMacierz(List<(string, string, double)> wyniki, List<string> pliki)
+    {
+        // TODO: wyswietlic naglowki kolumn
+        // TODO: dla kazdej pary znalezc wynik i wyswietlic
+        // TODO: oznaczyc pary powyzej progu jako [PLAGIAT?]
+        Console.WriteLine("\n=== WYNIKI ===");
+        foreach (var (a, b, podobienstwo) in wyniki)
+        {
+            string status = podobienstwo >= prog ? " [PLAGIAT?]" : "";
+            Console.WriteLine(a + " vs " + b + " = " + podobienstwo + "%" + status);
+        }
+    }
+
+    // zapisuje wyniki do pliku CSV
+    static void EksportujCSV(List<(string, string, double)> wyniki, string sciezkaWyjsciowa)
+    {
+        // TODO: StreamWriter, zapisac naglowek i kolejne wiersze
+    }
+}
---
+++ Serwer/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+class Server
+{
+    static int port = 8001;
+
+    static void Main()
+    {
+        TcpListener serwer = new TcpListener(IPAddress.Any, port);
+        serwer.Start();
+        Console.WriteLine("Serwer dziala na porcie " + port);
+
+        while (true)
+        {
+            TcpClient klient = serwer.AcceptTcpClient();
+            Console.WriteLine("Nowe polaczenie od klienta");
+            Task.Run(() => ObsluzKlienta(klient));
+        }
+    }
+
+    static void ObsluzKlienta(TcpClient klient)
+    {
+        NetworkStream stream = klient.GetStream();
+        BinaryReader reader = new BinaryReader(stream);
+        BinaryWriter writer = new BinaryWriter(stream);
+
+        Console.WriteLine("[SERWER] Czekam na parametry...");
+        int n = reader.ReadInt32();
+        int grainSize = reader.ReadInt32();
+        Console.WriteLine("[SERWER] Otrzymalem parametry: n=" + n + " grainSize=" + grainSize);
+        Thread.Sleep(1000);
+
+        Console.WriteLine("[SERWER] Czekam na tekst 1...");
+        string tekst1 = reader.ReadString();
+        Console.WriteLine("[SERWER] Otrzymalem tekst 1: " + tekst1);
+        Thread.Sleep(1500);
+
+        Console.WriteLine("[SERWER] Czekam na tekst 2...");
+        string tekst2 = reader.ReadString();
+        Console.WriteLine("[SERWER] Otrzymalem tekst 2: " + tekst2);
+        Thread.Sleep(2000);
+
+        Console.WriteLine("[SERWER] Licze slowa...");
+        Thread.Sleep(1000);
+        int iloscSlow1 = LiczSlowa(tekst1);
+        int iloscSlow2 = LiczSlowa(tekst2);
+
+        Console.WriteLine("[SERWER] Wyniki: tekst1=" + iloscSlow1 + " slow, tekst2=" + iloscSlow2 + " slow");
+        Thread.Sleep(500);
+
+        Console.WriteLine("[SERWER] Odsylam wyniki do klienta...");
+        writer.Write(iloscSlow1);
+        writer.Write(iloscSlow2);
+        Console.WriteLine("[SERWER] Gotowe!");
+
+        klient.Close();
+    }
+
+    // na razie liczy slowa, pozniej zastapiona przez PorownajPliki()
+    static int LiczSlowa(string tekst)
+    {
+        string[] slowa = tekst.Split(new char[] { ' ', '\t', '\n', '\r' },
+                                     StringSplitOptions.RemoveEmptyEntries);
+        return slowa.Length;
+    }
+
+    // glowna funkcja porownujaca dwa teksty
+    // zwraca podobienstwo w procentach (0.0 - 100.0)
+    static double PorownajPliki(string tekstA, string tekstB, int n)
+    {
+        // TODO: wywolac GenerujNgramy dla obu tekstow
+        // TODO: wywolac ObliczJaccard na zbiorach n-gramow
+        return 0.0;
+    }
+
+    // generuje zbior n-gramow dla podanego tekstu
+    static HashSet<string> GenerujNgramy(string tekst, int n)
+    {
+        // TODO: podzielic tekst na slowa
+        // TODO: przesuwac okno rozmiaru n po liscie slow
+        // TODO: kazdy n-gram dodac do HashSet
+        return new HashSet<string>();
+    }
+
+    // oblicza wspolczynnik Jaccarda dla dwoch zbiorow n-gramow
+    static double ObliczJaccard(HashSet<string> ngramyA, HashSet<string> ngramyB)
+    {
+        // TODO: obliczyc czesc wspolna (intersection)
+        // TODO: obliczyc sume zbiorow (union)
+        // TODO: podzielic i zwrocic wynik * 100
+        return 0.0;
+    }
+
+    // sprawdza czy wynik przekracza prog plagiatu
+    static bool CzyPlagiat(double podobienstwo, double prog)
+    {
+        // TODO: prosta comparacja podobienstwo >= prog
+        return false;
+    }
+}
